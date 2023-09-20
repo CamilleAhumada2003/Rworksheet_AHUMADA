e05308a11d5a63eab4379861713cedcdf92baeea
Camille Jean Ahumada <camillejean.ahumada@students.isatu.edu.ph>
2023-09-20 04:06:31 +0000

    initial commit

---
+++ Rworksheet_AHUMADA1.R
@@ -0,0 +1,28 @@
+#Vector
+
+age <- c (34, 28, 22, 36, 27, 18, 52, 39, 42, 29,
+            35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 
+            50, 37, 46, 25, 17, 37, 42, 53, 41,51, 
+            35, 24, 33, 41)
+
+#1 How many data points?
+  length(age)
+
+#2 Find the reciprocal of the values for age
+    reciprocal <-1/age
+    reciprocal
+
+#3 Assign also newAge <-c(age, 0 age)
+    newAge <-c(age, 0, age)
+    newAge
+
+#4 Sort the values for age
+    sortAge <-sort(age)
+    sortAge
+    
+#5  Minimum and maximum
+    minAge <- min(age)
+    minAge
+    maxAge <- max(age)
+    maxAge
+    
