dbffd871c566933bdce67163c5a6eabf9cfa404c
Camille Jean Ahumada <camillejean.ahumada@students.isatu.edu.ph>
2023-09-27 04:00:49 +0000

    commit

---
+++ Rworksheet_AHUMADA2.R
@@ -0,0 +1,47 @@
+#Worksheet #2
+
+#1 Create a vector using: operator
+
+#a. Sequence from -5 to 5. Write the R code and its output.
+#Describe its output.
+Negative5to5<-seq(-5,5)
+Negative5to5
+
+#b. x <- 1:7. What will be the value of x?
+x<-
+
+#2Create a vector using seq() function
+
+#a. seq(1, 3, by=0.2) # specify step size
+#Write the R script and its output. Describe the output.
+
+#3. A factory has a census of its workers. There are 50 workers in total. The following
+#list shows their ages: 
+34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27,
+22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 
+43, 53, 41, 51, 35, 24, 33, 41, 53, 40, 18, 44, 38, 
+41, 48, 27, 39, 19, 30, 61, 54, 58, 26, 18
+
+#a. Access 3rd element, what is the value?
+  
+#b. Access 2nd and 4th element, what are the values?
+  
+#c. Access all but the 4th and 12th element is not
+#included. Write the R script and its output.
+
+#4. *Create a vector x <- c("first"=3, "second"=0, "third"=9). Then named the
+vector, names(x).
+
+#a. Print the results. Then access x[c("first", "third")].
+Describe the output.
+
+#b. Write the code and its output.
+
+#5. Create a sequence x from -3:2.
+
+#a. Modify 2nd element and change it to 0;
+x[2] <- 0
+x
+#Describe the output.
+
+#b. Write the code and its output.
